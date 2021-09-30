2eccf40210e73ed4fbd445f9c476f772be981cb2
kevin <212634246@qq.com>
2021-09-30 16:50:09 +0800

    init repo

---
+++ src/main/java/com/example/mycommunity/MyCommunityApplication.java
@@ -0,0 +1,12 @@
+package com.example.mycommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCommunityApplication.class, args);
+    }
+}
