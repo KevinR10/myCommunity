35befa72d32b8925931e379bc46cdf946a127b14
kevin <212634246@qq.com>
2021-10-05 10:28:59 +0800

    Home List Function

--- src/main/java/com/example/mycommunity/controller/IndexController.java
+++ src/main/java/com/example/mycommunity/controller/IndexController.java
@@ -1,24 +1,31 @@
 package com.example.mycommunity.controller;
 
+import com.example.mycommunity.dto.QuestionDTO;
 import com.example.mycommunity.mapper.UserMapper;
 import com.example.mycommunity.model.User;
+import com.example.mycommunity.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private QuestionService questionService;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request) {
+    public String index(HttpServletRequest request,
+                        Model model) {
         //实现持久化登录状态
         Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0){
+        if (cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
                 if (cookie.getName().equals("token")) {
                     String token = cookie.getValue();
@@ -30,6 +37,8 @@ public class IndexController {
                 }
             }
         }
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions", questionList);
         return "index";
     }
 }
